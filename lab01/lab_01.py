0553bc9847a82915ec479621651b84682e9ea48c
hcmut-nguyuyen <100817714+hcmut-nguyuyen@users.noreply.github.com>
2024-09-24 08:44:19 +0700

    Create lab_01.py

---
+++ lab01/lab_01.py
@@ -0,0 +1,6 @@
+def on_forever():
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    basic.show_icon(IconNames.SMALL_SQUARE)
+    basic.show_icon(IconNames.DIAMOND)
+    basic.show_icon(IconNames.SQUARE)
+basic.forever(on_forever)
